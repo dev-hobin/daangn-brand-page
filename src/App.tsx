a56c93e67dd5ed5719b5e8b9415b6dd5bfe89e14
장호빈 <devhobin@gmail.com>
2024-03-07 23:10:08 +0900

    feat: 스크롤 가이드 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const introAnimation = ({
   const tl = gsap
     .timeline({
       onStart: onStart,
-      onComplete: onComplete,
     })
     .from('[data-question] [data-letter]', {
       visibility: 'hidden',
@@ -41,12 +40,54 @@ const introAnimation = ({
       {
         opacity: 0,
         yPercent: 100,
-        stagger: {
-          amount: 0.1,
-        },
+        stagger: { amount: 0.1 },
         ease: 'back.out(1.7)',
+        onComplete,
       },
       '-=25%',
+    )
+
+  return tl
+}
+
+const scrollGuideAnimation = () => {
+  const tl = gsap
+    .timeline()
+    .to('[data-scroll-guide]', { opacity: 1 })
+    .to(
+      '[data-scroll-guide] [data-text]',
+      {
+        keyframes: {
+          '0%': { opacity: 1 },
+          '50%': { opacity: 0.6 },
+          '75%': { opacity: 0.6, ease: 'power2.out' },
+          '100%': { opacity: 1 },
+          easeEach: 'power2.inOut',
+        },
+        repeat: -1,
+        yoyo: true,
+        repeatDelay: 1,
+        duration: 1.5,
+      },
+      '+=0.5',
+    )
+    .to(
+      '[data-scroll-guide] [data-icon]',
+      {
+        keyframes: {
+          '0%': { y: 0 },
+          '25%': { y: 12, ease: 'power2.in' },
+          '50%': { y: 0 },
+          '75%': { y: 12, ease: 'power2.in' },
+          '100%': { y: 0 },
+          easeEach: 'power2.out',
+        },
+        repeat: -1,
+        repeatDelay: 1,
+        yoyo: true,
+        duration: 1.5,
+      },
+      '<=',
     )
 
   return tl
@@ -68,7 +109,7 @@ function App() {
         document.body.style.overflow = 'auto'
         gsap.matchMediaRefresh()
       },
-    })
+    }).add(scrollGuideAnimation())
 
     new ScrollTrigger({
       trigger: scroller('intro'),
@@ -174,6 +215,34 @@ function App() {
               <span data-letter>&nbsp;당근이에요</span>
             </div>
           </h1>
+          <aside
+            className='absolute bottom-16 left-1/2 flex -translate-x-1/2 flex-col items-center opacity-0'
+            data-scroll-guide
+          >
+            <p data-text className='text-xl font-extrabold text-gray-400'>
+              아래로 내려보세요
+            </p>
+            <svg
+              data-icon
+              id='edubGENkp7x1'
+              xmlns='http://www.w3.org/2000/svg'
+              xmlnsXlink='http://www.w3.org/1999/xlink'
+              viewBox='0 0 24 8'
+              shapeRendering='geometricPrecision'
+              textRendering='geometricPrecision'
+              className='mx-auto mt-4 w-14'
+            >
+              <path
+                d='M5,8.793L17,21.95409L29,8.793v3.037174L17,24.991265L5,11.830174L5,8.793Z'
+                transform='matrix(1 0 0 0.49388-5.000012-4.342686)'
+              />
+              <path
+                d='M0,0h23.999976v8L0,8v-8Z'
+                transform='matrix(1.000001 0 0 1-.000012 0)'
+                fill='none'
+              />
+            </svg>
+          </aside>
         </section>
       </div>
       <div data-scroller='balloons'>
