52d797307e17030454e01b4896a248ee6ff823c1
장호빈 <devhobin@gmail.com>
2024-03-13 22:35:00 +0900

    feat: 만남의 순간들 섹션 애니메이션 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -501,7 +501,11 @@ function App() {
             </div>
 
             <div className='mt-[5.625rem] grid max-w-[60.625rem] grid-flow-row-dense grid-cols-2 grid-rows-[1fr_0.3fr_0.7fr_1fr] content-start gap-[1rem]'>
-              <div>
+              <article className='relative transition ease-out hover:scale-[1.01]'>
+                <h2 className='absolute left-0 top-[2.5rem] z-10 w-full text-center text-[1.75rem] font-bold'>
+                  좋은 물건을 구하고 <br />
+                  나누는 중고거래
+                </h2>
                 <Player
                   autoplay
                   loop
@@ -510,8 +514,8 @@ function App() {
                     preserveAspectRatio: 'xMidYMid slice',
                   }}
                 />
-              </div>
-              <div>
+              </article>
+              <div className='transition ease-out hover:scale-[1.01]'>
                 <video
                   autoPlay
                   loop
@@ -522,12 +526,18 @@ function App() {
                   <source src='/videos/moment_two.mp4' type='video/mp4' />
                 </video>
               </div>
-              <div className='row-span-2'>
+              <article className='relative row-span-2 transition ease-out hover:scale-[1.01]'>
+                <h2 className='absolute left-0 top-[2.5rem] z-10 w-full text-center text-[1.75rem] font-bold text-white'>
+                  걸어서 10분 동네 알바
+                </h2>
                 <video autoPlay loop playsInline muted>
                   <source src='/videos/moment_three.mp4' type='video/mp4' />
                 </video>
-              </div>
-              <div>
+              </article>
+              <article className='relative transition ease-out hover:scale-[1.01]'>
+                <h2 className='absolute left-0 top-[2.5rem] z-10 w-full text-center text-[1.75rem] font-bold text-[#212124]'>
+                  찐 이웃 추천 동네 가게
+                </h2>
                 <Player
                   autoplay
                   loop
@@ -536,8 +546,11 @@ function App() {
                     preserveAspectRatio: 'xMidYMid slice',
                   }}
                 />
-              </div>
-              <div>
+              </article>
+              <article className='relative transition ease-out hover:scale-[1.01]'>
+                <h2 className='absolute left-0 top-[2.5rem] z-10 w-full text-center text-[1.75rem] font-bold text-[#212124]'>
+                  지금 이 순간 동네 소식
+                </h2>
                 <Player
                   autoplay
                   loop
@@ -546,8 +559,11 @@ function App() {
                     preserveAspectRatio: 'xMidYMid slice',
                   }}
                 />
-              </div>
-              <div>
+              </article>
+              <article className='relative transition ease-out hover:scale-[1.01]'>
+                <h2 className='absolute left-0 top-[2.5rem] z-10 w-full text-center text-[1.75rem] font-bold text-[#212124]'>
+                  함께라서 더 즐거운 일들
+                </h2>
                 <Player
                   autoplay
                   loop
@@ -556,8 +572,8 @@ function App() {
                     preserveAspectRatio: 'xMidYMid slice',
                   }}
                 />
-              </div>
-              <div>
+              </article>
+              <div className='transition ease-out hover:scale-[1.01]'>
                 <video
                   autoPlay
                   loop
